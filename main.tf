c70b576ceee039ad2682d849e3a8ca93e56eb65d
Abhi0312-eve <abhijeet.sorte@eversana.com>
2026-08-08 00:30:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instanceType
   key_name               = var.keyName
-  vpc_security_group_ids = {var.securityGroupId}
+  vpc_security_group_ids = [var.securityGroupId]
 
   tags = {
     Name = var.instanceName
